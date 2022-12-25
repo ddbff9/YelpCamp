427ec4aad6be18ad1e3e9e9664429a91c029f580
Dan Bax <Dan.D.Bax@gmail.com>
2022-12-24 23:36:01 -0600

    Minor code refactoring after video lesson.

--- app.js
+++ app.js
@@ -2,9 +2,11 @@ const express = require('express');
 const path = require('path');
 const mongoose = require('mongoose');
 const Campground = require('./models/campground');
+const app = express();
 
+// Mongoose Setup:
+mongoose.set('strictQuery', true); //Added to stop showing the warning messae.
 mongoose.connect('mongodb://127.0.0.1:27017/yelp-camp');
-mongoose.set('strictQuery', true);
 
 const db = mongoose.connection;
 db.on("error", console.error.bind(console, "Connection Error:"));
@@ -12,15 +14,11 @@ db.once("open", () =>{
   console.log("Database Connected");
 });
 
-
-const app = express();
-
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname,'views'));
 
-app.get('/', (req,res)=>{
-  res.render('home');
-});
+//Mongoose Routes:
+app.get('/', (req,res)=>{res.render('home')});
 
 app.get('/makecampground', async (req, res)=>{
   const camp = new Campground({title:'My Backyard', description: 'Cheap camping.'})
