bf9bd2f733c8561cebf80e3bcb1e151f0a70b370
Dan Bax <Dan.D.Bax@gmail.com>
2022-12-24 22:13:05 -0600

    Video 410 -  Creating the Basic Express App.

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const app = express();
+const path = require('path');
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname,'views'));
 
 app.get('/', (req,res)=>{
-  res.send('HELLO FROM YELP CAMP!');
+  res.render('home');
 });
 
 app.listen(3000, (req, res)=>{
