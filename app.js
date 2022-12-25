8689c8bf3d92d84ccf0472b806874f2de02f1acc
Dan Bax <Dan.D.Bax@gmail.com>
2022-12-24 23:31:09 -0600

    411.  Campground Model  Basics

--- app.js
+++ app.js
@@ -1,12 +1,31 @@
 const express = require('express');
-const app = express();
 const path = require('path');
+const mongoose = require('mongoose');
+const Campground = require('./models/campground');
+
+mongoose.connect('mongodb://127.0.0.1:27017/yelp-camp');
+mongoose.set('strictQuery', true);
+
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "Connection Error:"));
+db.once("open", () =>{
+  console.log("Database Connected");
+});
+
+
+const app = express();
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname,'views'));
 
 app.get('/', (req,res)=>{
   res.render('home');
+});
+
+app.get('/makecampground', async (req, res)=>{
+  const camp = new Campground({title:'My Backyard', description: 'Cheap camping.'})
+  await camp.save();
+  res.send(camp);
 });
 
 app.listen(3000, (req, res)=>{
