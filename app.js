00622c219797a5be4291a0eef432d0145de52e3e
Dan Bax <Dan.D.Bax@gmail.com>
2022-12-26 13:08:56 -0600

    Video 426 - implemented ejsMate!

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const path = require('path');
 const mongoose = require('mongoose');
+const ejsMate = require('ejs-mate');
 const methodOverride = require('method-override');
 const Campground = require('./models/campground');
 const app = express();
@@ -15,6 +16,7 @@ db.once("open", () =>{
   console.log("Database Connected");
 });
 
+app.engine('ejs',ejsMate);
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname,'views'));
 
