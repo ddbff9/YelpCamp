452eea866a3f064b6269a9cf5ca937cc9bc2b892
Dan Bax <Dan.D.Bax@gmail.com>
2022-12-26 13:49:44 -0600

    Video 430: Added images

--- seeds/index.js
+++ seeds/index.js
@@ -19,9 +19,13 @@ const seedDB = async () => {
   await Campground.deleteMany({});
   for(let i=0; i<50; i++){
     const random1000 = Math.floor(Math.random(0)*1000);
+    const price = Math.floor(Math.random()*20) + 10
     const camp = new Campground({
       location: `${cities[random1000].city}, ${cities[random1000].state}`,
-      title: `${sample(descriptors)} ${sample(places)}`
+      title: `${sample(descriptors)} ${sample(places)}`,
+      image: 'https://source.unsplash.com/random/900%C3%97700/?camping',
+      description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus cum ea officiis eius vitae libero quos, optio omnis harum dicta saepe consequuntur consectetur voluptate sint explicabo non excepturi aperiam voluptatibus.',
+      price
     });
     await camp.save();
   }
