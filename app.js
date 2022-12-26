0fbfed3f68f22275dd6e2d287bd479e5d974a157
Dan Bax <Dan.D.Bax@gmail.com>
2022-12-25 20:56:55 -0600

    Video 415 - Campground Add and Create

--- app.js
+++ app.js
@@ -17,6 +17,8 @@ db.once("open", () =>{
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname,'views'));
 
+app.use(express.urlencoded({extended: true}));
+
 //Mongoose Routes:
 app.get('/', (req,res)=>{res.render('home')});
 
@@ -25,11 +27,22 @@ app.get('/campgrounds', async (req,res)=>{
   res.render('campgrounds/index', { campgrounds });
 });
 
+app.get('/campgrounds/new', (req,res)=>{
+  res.render('campgrounds/new');
+});
+
+app.post('/campgrounds', async (req,res)=>{
+  const campground = new Campground(req.body.campground);
+  await campground.save();
+  res.redirect(`/campgrounds/${campground._id}`);
+})
+
 app.get('/campgrounds/:id', async (req, res)=>{
   const {id} = req.params;
   const campground = await Campground.findById(id);
   res.render('campgrounds/show', { campground });
-})
+});
+
 
 app.listen(3000, (req, res)=>{
   console.log('Serving on port 3000')
