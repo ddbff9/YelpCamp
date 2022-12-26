9b38352f15f5448839caa1c43ab851c9577a88ff
Dan Bax <Dan.D.Bax@gmail.com>
2022-12-25 21:13:55 -0600

    Video 416 - Campground Edit and Update func added.

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const path = require('path');
 const mongoose = require('mongoose');
+const methodOverride = require('method-override');
 const Campground = require('./models/campground');
 const app = express();
 
@@ -18,6 +19,7 @@ app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname,'views'));
 
 app.use(express.urlencoded({extended: true}));
+app.use(methodOverride('_method'));
 
 //Mongoose Routes:
 app.get('/', (req,res)=>{res.render('home')});
@@ -43,6 +45,17 @@ app.get('/campgrounds/:id', async (req, res)=>{
   res.render('campgrounds/show', { campground });
 });
 
+app.get('/campgrounds/:id/edit', async (req,res)=>{
+  const {id} = req.params;
+  const campground = await Campground.findById(id);
+  res.render('campgrounds/edit', { campground });
+});
+
+app.put('/campgrounds/:id', async (req,res)=>{
+  const {id} = req.params;
+  const campground = await Campground.findByIdAndUpdate(id,{...req.body.campground});
+  res.redirect(`/campgrounds/${campground._id}`);
+});
 
 app.listen(3000, (req, res)=>{
   console.log('Serving on port 3000')
